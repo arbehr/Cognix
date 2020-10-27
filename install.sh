a5687c3055e20b7fcfd876d0df76a354e0d91cc3
Andre Rolim Behr <arbehr@inf.ufrgs.br>
2020-10-27 10:09:47 -0100

    Correção do locale para Portugal

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo -u postgres psql -c "CREATE USER cognitiva WITH PASSWORD 'rep@cognitiva'"
 echo "Tentando deletar a base de dados antiga..."
 sudo -u postgres dropdb repositorio
 echo "#Criando base local..."
-sudo -u postgres createdb -E UTF8 -T template0 --locale=pt_BR.utf8 -O cognitiva repositorio
+sudo -u postgres createdb -E UTF8 -T template0 --locale=pt_PT.utf8 -O cognitiva repositorio
 
 echo "#Criando as tabelas..."
 sudo -u postgres psql repositorio < ./src/main/resources/schema.sql
