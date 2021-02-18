3034fd7958671270597ce3f35ad1bb3316503a25
Andre Rolim Behr <arbehr@inf.ufrgs.br>
2021-02-18 09:27:11 -0100

    Remoção do hífen no nome para o banco de dados

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 echo "Executando o script de inicialização da base de dados..."
 echo "#Creating user..."
-sudo -u postgres psql -c "CREATE USER re-mar WITH PASSWORD 'teste123'"
+sudo -u postgres psql -c "CREATE USER remar WITH PASSWORD 'teste123'"
 echo "Tentando deletar a base de dados antiga..."
 sudo -u postgres dropdb repositorio
 echo "#Criando base local..."
-sudo -u postgres createdb -E UTF8 -T template0 --locale=pt_PT.utf8 -O re-mar repositorio
+sudo -u postgres createdb -E UTF8 -T template0 --locale=pt_PT.utf8 -O remar repositorio
 
 echo "#Criando as tabelas..."
 sudo -u postgres psql repositorio < ./src/main/resources/schema.sql
--- src/main/resources/postgres_schema.sql
+++ src/main/resources/postgres_schema.sql
@@ -3,11 +3,11 @@ ALTER TABLE documents ADD UNIQUE (obaa_entry);
 
 -- Insert users
 INSERT INTO users (login, password, name, permissions, role)
-    VALUES ('admin', md5('teste'), 'Administrador Geral', 'PERM_CREATE_DOC,PERM_MANAGE_DOC,PERM_VIEW,PERM_MANAGE_USERS', 'root');
+    VALUES ('admin@uac.pt', md5('teste'), 'Administrador Geral', 'PERM_CREATE_DOC,PERM_MANAGE_DOC,PERM_VIEW,PERM_MANAGE_USERS', 'root');
 INSERT INTO users (login, password, name, permissions, role)
     VALUES ('anonymous@uac.pt', md5('anonymous'), 'Usuário anônimo', '', 'reader');
 
-ALTER TABLE documents OWNER TO re-mar;
-ALTER TABLE files OWNER TO re-mar;
-ALTER TABLE users OWNER TO re-mar;
-ALTER TABLE subject OWNER TO re-mar;
+ALTER TABLE documents OWNER TO remar;
+ALTER TABLE files OWNER TO remar;
+ALTER TABLE users OWNER TO remar;
+ALTER TABLE subject OWNER TO remar;
